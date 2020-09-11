179bb6261397f6405fbd79711068afa68c8315d1
mikaelaliwanag <mikaela301@gmail.com>
2020-09-11 16:00:23 -0700

    Add props id in Card.js

--- src/Card.js
+++ src/Card.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Card = (props) => {
     return (
         <div className='tc bg-washed-red dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='robot' src='https://robohash.org/yoooo?200x200'/>
+            <img alt='robot' src={`https://robohash.org/${props.id}?200x200`} />
             <div>
                 <h2>{props.name}</h2>
                 <p>{props.email}</p>
